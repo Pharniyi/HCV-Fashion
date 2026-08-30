7d3b7590277d515d732ff91b83340fd16cbce461
Pharniyi <faniyitimi@gmail.com>
2026-08-30 22:57:38 +0100

    updated product  pages

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-// import useParams from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+//import {products} from '../src/assets'
+import {Link} from 'react-router-dom'
+
 const ProductPage = () => {
-  const {productId} = useParams();
+  //const {productId} = useParams();
+  const product =products.find((item) => item._id ===id)
+  const [image, setImage] =useState(null)
+  const [size, setSize] = useState(null)
+
+  useEffect(() => {
+    if (product){
+      setImage(product.image[0])
+    }
+  },[product])
   return (
-    <div>ProductPage</div>
+    product && (
+    <div>
+      <div>
+        {product.image.map((item,index)=> (
+          <div key={index}>
+            <img onClick={ () => setImage(item)} src={item} alt={'productimage'} className="object-cover aspect-square" />
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <img src={image} alt="productimage" />
+      </div>
+    </div>
+
+    {/*product info*/}
+    <div>
+      <h3>{product.name}</h3>
+      <div>
+        <div>
+          <TbStarFilled/>
+          <TbStarFilled/>
+          <TbStarFilled/>
+          <TbStarHalfFilled/>
+        </div>
+        <p>(22)</p>
+      </div>
+      <div>
+        <h3>{currency} {product.price}.00</h3>
+        <h4>{currency} {product.price}.00</h4>
+        <h1></h1>
+      </div>
+      <p className="max-w-[555px]">{product.description}</p>
+      <div>
+        <div>
+          {[...product.sizes].sort((a,b) => {
+            const order= ['S', 'M', 'L', 'XL']
+          })}
+        </div>
+      </div>
+    </div>
+    )
   )
 }
 
